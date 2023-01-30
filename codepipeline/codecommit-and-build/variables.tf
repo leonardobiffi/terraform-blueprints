d6a874a0caa2585a68159d888e0b6d69a7f20fc3
Leonardo Biffi <leonardobiffi@outlook.com>
2023-01-30 09:01:20 -0400

    add codepipeline/codecommit-and-build

---
+++ codepipeline/codecommit-and-build/variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  description = "The AWS region to use"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "app" {
+  description = "The name of the app"
+  type        = string
+}
+
+variable "env" {
+  description = "The environment to set"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "The name of the repository"
+  type        = string
+}
+
+variable "repo_branch" {
+  description = "The branch of the repository"
+  type        = string
+}
+
+variable "s3_bucket_artifact" {
+  description = "The S3 bucket to store the artifact"
+  type        = string
+}
+
+variable "s3_bucket_artifact_arn" {
+  description = "The ARN S3 bucket to store the artifact"
+  type        = string
+}
+
+variable "environment_build_variables" {
+  description = "The environment variables to use"
+  type        = list(map(string))
+}
+
+variable "build_image" {
+  description = "The name of the build image to use"
+  type        = string
+  default     = "aws/codebuild/standard:5.0"
+}
+
+variable "build_compute_type" {
+  description = "The compute type to use"
+  type        = string
+  default     = "BUILD_GENERAL1_SMALL"
+}
+
+variable "tags" {
+  description = "Additional tags to use"
+  type        = map(string)
+  default     = {}
+}
