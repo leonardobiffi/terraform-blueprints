d73f523859ce696e51631706cfcd7a1864c82b57
Leonardo Biffi <leonardobiffi@outlook.com>
2025-06-17 07:25:53 -0400

    fix: ingress security group

--- ecs/fargate-capacity-provider/vpc.tf
+++ ecs/fargate-capacity-provider/vpc.tf
@@ -10,10 +10,16 @@ module "security_group_ecs" {
   ingress_rules       = length(var.ingress_cidr_blocks) > 0 ? ["http-${var.container_port}-tcp"] : []
 
   ingress_with_source_security_group_id = [
-    for i in var.source_security_group_ids :
+    for i in var.source_security_group_ids : var.container_port == 80 || var.container_port == 8080 ?
     {
       description              = "Allow access by security group"
       rule                     = "http-${var.container_port}-tcp"
+      source_security_group_id = i
+      } : {
+      description              = "Allow access by security group"
+      from_port                = var.container_port
+      to_port                  = var.container_port
+      protocol                 = "tcp"
       source_security_group_id = i
     }
   ]
